5665f2a9259b1e27b18ff58d2fd54ed737a6f612
Éden Silva <91293090+Eden-Daniel@users.noreply.github.com>
2022-08-12 22:09:56 -0300

    atualizado o main.js
    Adicionado funcionalidade com teclado e logica de verificação de possíveis problemas.

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function tocarSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-};
+function tocarSom(elementoAudio) {
+    elemento = document.querySelector(elementoAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }    
+}
 
 const listaTeclas = document.querySelectorAll('.tecla');
 
@@ -11,5 +17,15 @@ for (let contador = 0; contador < listaTeclas.length; contador++){
 
     tecla.onclick = function () {
         tocarSom(idAudio);
-    };
-};
+    }
+
+    tecla.onkeydown = function(evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+}
